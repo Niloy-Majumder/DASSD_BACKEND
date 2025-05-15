6bb8c637afb27c534c535bfaf7ad412d626ab4d2
Niloy Majumder <niloym@cdac.in>
2025-05-15 10:37:30 +0530

    movies apis

---
+++ movies.js
@@ -0,0 +1,53 @@
+const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    director: "Christopher Nolan",
+    releaseYear: 2010,
+    genre: "Science Fiction",
+    rating: 8.8,
+    description:
+      "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.",
+  },
+  {
+    id: 2,
+    title: "The Shawshank Redemption",
+    director: "Frank Darabont",
+    releaseYear: 1994,
+    genre: "Drama",
+    rating: 9.3,
+    description: "Two imprisoned ",
+  },
+  {
+    id: 3,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    releaseYear: 1972,
+    genre: "Crime",
+    rating: 9.2,
+    description:
+      "An organized crime dynasty's aging patriarch transfers control of his clandestine empire to his reluctant son.",
+  },
+  {
+    id: 4,
+    title: "The Dark Knight",
+    director: "Christopher Nolan",
+    releaseYear: 2008,
+    genre: "Action",
+    rating: 9,
+    description:
+      "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.",
+  },
+  {
+    id: 5,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    releaseYear: 1994,
+    genre: "Crime",
+    rating: 8.9,
+    description:
+      "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+  },
+];
+
+module.exports = movies;
